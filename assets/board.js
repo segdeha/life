ed8a092b82c329b6851b388f00d4c0e6c8516c63
Andrew Hedges <andrew@hedges.name>
2021-04-10 10:55:20 -0700

    add a couple of clarifying comments

--- assets/board.js
+++ assets/board.js
@@ -23,9 +23,12 @@ class Board {
     setup() {
         // create a Cell for each row x column
         for (let r = 0; r < this.rows; r += 1) {
-            this.grid[r] = []
+            // create the row in the DOM
             const html = `<div class="row row-${r}"></div>`
             this.node.insertAdjacentHTML('beforeend', html)
+            // create a place to store the cells in memory
+            this.grid[r] = []
+            // create the cells
             for (let c = 0; c < this.cols; c += 1) {
                 const cell = new Cell(r, c, this.isRandomlyAlive())
                 this.grid[r][c] = cell
